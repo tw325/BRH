7243577b22333fb940bd34f996e5d616286ab413
Tyler Wang <twang.123@live.com>
2016-09-16 23:49:37 -0400

    added index and contact page

--- src/app.py
+++ src/app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/index")
+def index():
+    return "this is the index"
+
+@app.route("/contact")
+def contact():
+    return "Contact us!"
+
 if __name__ == "__main__":
     app.run()
